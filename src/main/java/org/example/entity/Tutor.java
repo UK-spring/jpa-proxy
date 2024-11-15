83a1f54ce9be13465eaa27ad14b45ad8db46758d
UK <hwangwonuk94@gmail.com>
2024-11-16 03:40:40 +0900

    feat: 초기 Entity 세팅

---
+++ src/main/java/org/example/entity/Tutor.java
@@ -0,0 +1,37 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tutor")
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    public Tutor() {
+    }
+
+    public Tutor(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
