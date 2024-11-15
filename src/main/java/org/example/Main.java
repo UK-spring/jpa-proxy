bda44066e19eee2d3eadf3d5051d79575ff840ce
UK <hwangwonuk94@gmail.com>
2024-11-16 04:57:32 +0900

    feat: find(), getReference()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.example.entity.Company;
+import org.example.entity.Tutor;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +18,21 @@ public class Main {
         transaction.begin();
 
         try {
+
+            Tutor tutor = new Tutor("wonuk");
+            em.persist(tutor);
+
+            // 영속성 컨텍스트 초기화
+            em.flush();
+            em.clear();
+
+            // em.find()
+             Tutor findTutor = em.find(Tutor.class, tutor.getId());
+
+            // Proxy 객체 조회
+//            Tutor proxyTutor = em.getReference(Tutor.class, tutor.getId());
+//            System.out.println("proxyTutor.getName() = " + proxyTutor.getName());
+//            System.out.println("proxyTutor.getClass() = " + proxyTutor.getClass());
 
             transaction.commit();
         } catch (Exception e) {
