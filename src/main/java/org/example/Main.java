4e70d1c30526e328f1b1a477cb9629096fc3a68e
UK <hwangwonuk94@gmail.com>
2024-11-16 05:30:19 +0900

    feat: optimize imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import org.example.entity.Company;
 import org.example.entity.Tutor;
 
 public class Main {
